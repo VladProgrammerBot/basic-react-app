74694ef75128815814733ad8c78376f37108ae19
VladProgrammerBot <user.programmer29845@gmail.com>
2025-10-03 20:24:21 +0300

    ..

--- src/data/sidebar.tsx
+++ src/data/sidebar.tsx
@@ -1,11 +1,5 @@
-import { IoMove } from "react-icons/io5";
-import { GoPencil } from "react-icons/go";
-import { IoSearchOutline } from "react-icons/io5";
-import { BsPlusLg } from "react-icons/bs";
-import { RxQuestionMark } from "react-icons/rx";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { HiPencilAlt } from "react-icons/hi";
-import { FaPen } from "react-icons/fa";
 import { LuTextCursor } from "react-icons/lu";
 import { IoSearch } from "react-icons/io5";
 import { IoMdMove } from "react-icons/io";
