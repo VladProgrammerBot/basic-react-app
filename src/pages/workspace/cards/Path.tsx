53cde75e98e505bae7cd3ab4d03f13cee504b976
VladProgrammerBot <user.programmer29845@gmail.com>
2025-10-30 20:46:04 +0200

    ts error fixed

--- src/pages/workspace/cards/Path.tsx
+++ src/pages/workspace/cards/Path.tsx
@@ -1,6 +1,6 @@
 import { useFolders } from "@/hooks/useFolders";
 import store from "@/state/store";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import { RiArrowRightSLine } from "react-icons/ri";
 
