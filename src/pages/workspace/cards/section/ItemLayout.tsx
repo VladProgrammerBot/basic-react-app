ffe7d180d04b60a5401ae1185f35c12bc2ab04a3
VladProgrammerBot <user.programmer29845@gmail.com>
2025-11-13 19:31:26 +0200

    types fix

--- src/pages/workspace/cards/section/ItemLayout.tsx
+++ src/pages/workspace/cards/section/ItemLayout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react"
 
-export const ItemLayout = ({children, className, filled, onClick} : PropsWithChildren & {className?: string, filled?: boolean, onClick?: () => void}) => {
+export const ItemLayout = ({children, className, onClick} : PropsWithChildren & {className?: string, filled?: boolean, onClick?: () => void}) => {
     return (
         <div onClick={onClick} className={`hover:bg-neutral-300 dark:hover:bg-neutral-500/10 duration-150 w-full min-h-14 border-b-1 border-neutral-400/20 cursor-pointer ${className}`}>
             {children}
