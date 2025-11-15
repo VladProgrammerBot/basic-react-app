f3972704cb9f2612d980b7594914a5d2463efcdb
VladProgrammerBot <user.programmer29845@gmail.com>
2025-11-15 22:50:24 +0200

    message interface

--- src/pages/workspace/Edit.tsx
+++ src/pages/workspace/Edit.tsx
@@ -4,6 +4,8 @@ import { Path } from "./cards/header/Path";
 import { useFolders } from "@/hooks/useFolders";
 import store from "@/state/store";
 import { Footer } from "./cards/footer";
+import { IoMdAlert } from "react-icons/io";
+import { IoMdClose } from "react-icons/io";
 
 import { Bar } from "./cards/Bar";
 
@@ -32,6 +34,16 @@ export const Edit = () => {
           </div>
         </>
       )}
+      <div className="p-2 fixed bottom-0 z-20 right-0">
+
+      <div className="flex p-4 items-center animate-fade-in gap-2 border-1  rounded-md border-green-500 bg-green-800/90">
+        <IoMdAlert className="text-xl" />
+        <p className="text-sm flex-1 mr-4 sm:mr-8">Folder Added success, now you can move inside it</p>
+        <button className="cursor-pointer">
+          <IoMdClose />
+        </button>
+      </div>
+      </div>
     </div>
   );
 };
--- src/pages/workspace/cards/footer.tsx
+++ src/pages/workspace/cards/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <div className="text-sm opacity-50 text-center pb-4 pt-[60vh]">
+        <div className="text-sm opacity-50 text-center pb-4 pt-[20vh]">
             2025, Made in Ukraine by Vlad
         </div>
     )
