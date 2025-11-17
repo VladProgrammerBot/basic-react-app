4abbb828969db57da53ec59a44aab60de32e6a37
VladProgrammerBot <user.programmer29845@gmail.com>
2025-11-17 15:41:58 +0200

    fix: types

--- src/pages/workspace/Edit.tsx
+++ src/pages/workspace/Edit.tsx
@@ -6,7 +6,7 @@ import store from "@/state/store";
 import { Footer } from "./cards/footer";
 
 import { Bar } from "./cards/Bar";
-import { Alerts } from "./cards/section/Alerts";
+// import { Alerts } from "./cards/section/Alerts";
 
 export const Edit = () => {
   const folders = store.use.folders();
