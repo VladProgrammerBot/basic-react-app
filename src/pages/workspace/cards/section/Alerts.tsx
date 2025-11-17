b24c2679735177a5094f30cb1539a39ee659a04b
VladProgrammerBot <user.programmer29845@gmail.com>
2025-11-17 16:14:46 +0200

    ui fixing + message theme supp

--- src/pages/workspace/cards/section/Alerts.tsx
+++ src/pages/workspace/cards/section/Alerts.tsx
@@ -2,8 +2,8 @@ import { IoMdAlert, IoMdClose } from "react-icons/io"
 
 export const Alerts = () => {
     return (
-        <div className="p-2 fixed text-white bottom-0 z-20 right-0">
-        <div className="flex p-4 items-center animate-fade-in gap-2 border-1  rounded-md border-green-500 bg-green-800/90">
+        <div className="p-4 fixed dark:text-white bottom-0 z-20 right-0">
+        <div className="flex p-4 items-center animate-fade-in gap-2 border-1 bg-green-200 shadow-md shadow-neutral-300 dark:shadow-neutral-950 rounded-md border-green-500 dark:bg-green-800/90">
           <IoMdAlert className="text-xl" />
           <p className="text-sm flex-1 mr-4 sm:mr-8">Folder Added success, now you can move inside it</p>
           <button className="cursor-pointer">
