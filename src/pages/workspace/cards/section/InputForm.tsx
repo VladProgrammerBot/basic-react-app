d976aca9e469d4c099dc563c6eb662f3c5a265fb
VladProgrammerBot <user.programmer29845@gmail.com>
2025-11-22 23:45:57 +0200

    fix ui

--- src/pages/workspace/cards/section/InputForm.tsx
+++ src/pages/workspace/cards/section/InputForm.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button"
 import store from "@/state/store";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const InputForm = ({ placeholder, defaultValue, cancelFunc, submitFunc, submitTitle }: {
     placeholder?: string
@@ -11,6 +11,7 @@ export const InputForm = ({ placeholder, defaultValue, cancelFunc, submitFunc, s
 }) => {
     const ref = useRef<HTMLTextAreaElement | null>(null);
     const childrensId = store.use.childrensId()
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         if (defaultValue && ref.current)
@@ -19,11 +20,14 @@ export const InputForm = ({ placeholder, defaultValue, cancelFunc, submitFunc, s
     }, [])
 
     return (
-        <div className={`p-2 w-full ${childrensId.length !== 0 && "border-t-1"} border-neutral-700`}>
+        <div className={`p-2 w-full ${childrensId.length !== 0 && "border-t-1"} border-neutral-300 dark:border-neutral-700`}>
             <textarea ref={ref} placeholder={placeholder ?? "Enter text"} className="px-4 py-2 w-full mb-2 outline-none resize-none" />
             <div className="flex justify-end gap-2">
                 <Button onClick={cancelFunc} variant={"outline"}>Cancel</Button>
-                <Button onClick={() => ref.current && submitFunc(ref.current.value, null)}>{submitTitle}</Button>
+                <Button onClick={() => {
+                    setIsLoading(true)
+                    ref.current && submitFunc(ref.current.value, null)
+                }}>{isLoading && <span className="auth-loader"></span>}{submitTitle}</Button>
             </div>
         </div>
     )
