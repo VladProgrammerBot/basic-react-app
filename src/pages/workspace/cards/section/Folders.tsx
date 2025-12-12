417160067f9caf0028f735bcb6ab39fc2752f409
VladProgrammerBot <user.programmer29845@gmail.com>
2025-12-12 22:42:04 +0200

    jic

--- src/pages/workspace/cards/section/Folders.tsx
+++ src/pages/workspace/cards/section/Folders.tsx
@@ -9,13 +9,14 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 import { useKeyboard } from "@/hooks/folders/useKeyboard";
+import { Button } from "@/components/ui/button";
 
 export const Folders = () => {
   const { childrensData, generateFolders, buttons } = useFolders()
   const { addFolder } = useFolderManipulation()
   const setMode = store(state => state.setMode)
   const mode = store.use.mode()
-  
+
   useKeyboard()
 
   return (
@@ -37,10 +38,10 @@ export const Folders = () => {
         if (button.cond) {
           return (
             <Tooltip key={index}>
-              <TooltipTrigger onClick={button.func} className={`flex-1 rounded-m flex items-center 
-            justify-center text-center gap-1 py-2 px-4 cursor-pointer duration-150 bg-neutral-200 
-            dark:bg-neutral-800 hover:bg-neutral-300 dark:hover:bg-neutral-700`}>
-                {button.icon} {button.title}
+              <TooltipTrigger className="flex-1" onClick={button.func} >
+                <Button className="w-full">
+                  {button.icon} {button.title}
+                </Button>
               </TooltipTrigger>
               <TooltipContent>
                 {button.Hotkeys}
@@ -51,6 +52,14 @@ export const Folders = () => {
           return ""
         }
       })}</div>}
+      {/* <button className="flex items-center px-5 py-2 text-sm font-bold uppercase tracking-widest bg-neon-cyan text-gray-900 border-2 border-cyan-400 rounded-sm shadow-xl hover:bg-cyan-400 transition-all duration-300 action-btn glow-effect">
+            <svg viewBox="0 0 24 24" className="w-5 h-5 mr-3" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <line x1="12" y1="5" x2="12" y2="19"></line>
+                <line x1="5" y1="12" x2="19" y2="12"></line>
+            </svg>
+            <span className="btn-text">НОВИЙ ЕЛЕМЕНТ</span>
+        </button> */}
     </div>
+    
   );
 };
