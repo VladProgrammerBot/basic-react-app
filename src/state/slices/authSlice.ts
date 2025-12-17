65b650030b07f19967ab5b32b6b5b3689fbd0b5c
VladProgrammerBot <user.programmer29845@gmail.com>
2025-12-17 16:14:12 +0200

    fixed styles toggling bug2

--- src/state/slices/authSlice.ts
+++ src/state/slices/authSlice.ts
@@ -4,6 +4,6 @@ import type { StateCreator } from "zustand";
 export const createAuthSlice: StateCreator<authSlice> = (set) => ({
     isLogin: true,
     setIsLogin: (state) => set({ isLogin: state }),
-    isStyled: localStorage.getItem("isNotStyled") === "false" || true,
+    isStyled: localStorage.getItem("isNotStyled") === "false" || false,
     setIsStyled: () => set((state) => ({ isStyled: !state.isStyled })),
 })
