e0d83f9e8e1dd94b05a186ed781043f716e24944
VladProgrammerBot <user.programmer29845@gmail.com>
2025-12-23 15:37:31 +0200

    new guide

--- src/pages/workspace/Edit.tsx
+++ src/pages/workspace/Edit.tsx
@@ -6,7 +6,7 @@ import store from "@/state/store";
 import { Bar } from "./cards/Bar";
 import { Messages } from "./cards/section/Messages";
 import { useEdit } from "@/hooks/folders/useEdit";
-import { Guide } from "./cards/Guide";
+import { Guide } from "./cards/Guide/Guide";
 
 export const Edit = () => {
   const folders = store.use.folders();
