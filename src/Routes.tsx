b1e3dd26efcce7cfc589631bb5d5c535fbc71e9f
VladProgrammerBot <user.programmer29845@gmail.com>
2026-01-22 18:15:10 +0200

    new experement pages

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,13 @@
 import { createBrowserRouter } from "react-router";
-import { Home } from "./pages/Home.tsx";
+import { Home } from "./pages/Home/Home.tsx";
 import { AuthLayout } from "./pages/auth/AuthLayout.tsx";
 import { Edit } from "./pages/workspace/Edit.tsx";
 import { AuthForm } from "./pages/auth/AuthForm.tsx";
 import { About } from "./pages/about/About.tsx";
 import { GlobalLayout } from "./pages/GlobalLayout.tsx";
+import { Experements } from "./pages/experemental/Experement.tsx";
+import { NGA } from "./pages/experemental/NewGuideAnim.tsx";
+import { Guide } from "./pages/workspace/cards/Guide/Guide.tsx";
 // import { Experements } from "./Experements.tsx";
 
 export const router = createBrowserRouter([
@@ -20,10 +23,20 @@ export const router = createBrowserRouter([
         path: "workspace",
         Component: Edit,
       },
-      // {
-      //   path: "experements",
-      //   Component: Experements,
-      // },
+      {
+        path: "experements",
+        Component: Experements,
+        children: [
+          {
+            path: "nga",
+            Component: NGA,
+          },
+          {
+            path: "guide",
+            Component: Guide,
+          },
+        ],
+      },
       {
         path: "about",
         Component: About,
--- src/pages/experemental/Experement.tsx
+++ src/pages/experemental/Experement.tsx
@@ -1,7 +1,16 @@
-export const Experement = () => {
-    return (
-        <div>
-            Exp
-        </div>
-    )
-}
+import { Outlet } from "react-router";
+import { Link } from "react-router";
+
+export const Experements = () => {
+  
+
+  return (
+    <div className="p-4">
+      <div className="space-x-4">
+        <Link to="guide">Guide</Link>
+        <Link to="nga">New anim</Link>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
