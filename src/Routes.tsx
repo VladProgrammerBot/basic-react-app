d68cfc02ee21782c8cbd3b9eed6ed39ea0bd042b
VladProgrammerBot <user.programmer29845@gmail.com>
2026-01-23 12:33:30 +0200

    removed test components

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -5,14 +5,6 @@ import { Edit } from "./pages/workspace/Edit.tsx";
 import { AuthForm } from "./pages/auth/AuthForm.tsx";
 import { About } from "./pages/about/About.tsx";
 import { GlobalLayout } from "./pages/GlobalLayout.tsx";
-import { Experements } from "./pages/experemental/Experement.tsx";
-import { NGA } from "./pages/experemental/NewGuideAnim.tsx";
-import { Guide } from "./pages/workspace/cards/Guide/Guide.tsx";
-import { WithoutMarkdown } from "./pages/experemental/WithoutMarkdown.tsx";
-import { OnluMD } from "./pages/experemental/OnlyMD.tsx";
-import { SimpleMd } from "./pages/experemental/SimpleMd.tsx";
-import { SimpleMd2 } from "./pages/experemental/SimpleMd2.tsx";
-// import { Experements } from "./Experements.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -26,36 +18,6 @@ export const router = createBrowserRouter([
       {
         path: "workspace",
         Component: Edit,
-      },
-      {
-        path: "experements",
-        Component: Experements,
-        children: [
-          {
-            path: "nga",
-            Component: NGA,
-          },
-          {
-            path: "guide",
-            Component: Guide,
-          },
-          {
-            path: "without-markdown",
-            Component: WithoutMarkdown,
-          },
-          {
-            path: "only-md",
-            Component: OnluMD,
-          },
-          {
-            path: "simple-md-1",
-            Component: SimpleMd,
-          },
-          {
-            path: "simple-md-2",
-            Component: SimpleMd2,
-          },
-        ],
       },
       {
         path: "about",
--- src/pages/experemental/SimpleMd2.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactMarkdown from "react-markdown";
-import { guideMarkdownSteps } from "../workspace/cards/Guide/GuideMarkdown";
-
-export const SimpleMd2: React.FC = () => {
-  return <ReactMarkdown>{guideMarkdownSteps[0]}</ReactMarkdown>;
-};
