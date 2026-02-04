b74bb389ceb2e1b6dfee4a668cdb87a9210fbfc1
VladProgrammerBot <user.programmer29845@gmail.com>
2026-02-04 22:15:04 +0200

    fix homepage header bg

--- src/pages/Home/Navigation.tsx
+++ src/pages/Home/Navigation.tsx
@@ -18,7 +18,7 @@ export const Navigation = ({ isLoggedIn, onSignIn }: NavigationProps) => {
         damping: 15,
         stiffness: 100,
       }}
-      className="flex fixed w-full z-10 items-center justify-between mb-12 left-0 top-0 bg-white/70 dark:bg-neutral-900/10 p-4 backdrop-blur-lg border-b border-neutral-200/50 dark:border-transparent"
+      className="flex fixed w-full z-10 items-center justify-between mb-12 left-0 top-0 p-4 ackdrop-blur-lg border-b border-neutral-200/50 dark:border-transparent"
     >
       <div className="flex items-center gap-3">
         <div className="w-10 h-10 rounded-lg bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
@@ -32,12 +32,12 @@ export const Navigation = ({ isLoggedIn, onSignIn }: NavigationProps) => {
       <div className="flex items-center gap-2">
         {!isLoggedIn && (
           <>
-            <Button variant="ghost" size="icon" onClick={onSignIn}>
+            <Button variant="default" size="icon" onClick={onSignIn}>
               Sign In
-            </Button>
-            <p className="text-xs bg-neutral-500/10 rounded-md px-2 py-1 border-1 border-neutral-500/30 text-black/50 dark:text-white/50">
-              Press <b>L</b>
+            <p className="text-xs text-black/50 dark:text-white/50">
+              Press L
             </p>
+            </Button>
           </>
         )}
       </div>
