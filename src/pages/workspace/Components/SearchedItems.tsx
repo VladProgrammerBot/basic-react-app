d2fa41abf7ca692f7589c3608b811465908ebf41
VladProgrammerBot <user.programmer29845@gmail.com>
2026-03-05 15:49:24 +0200

    fixed items selecting in search mod

--- src/pages/workspace/Components/SearchedItems.tsx
+++ src/pages/workspace/Components/SearchedItems.tsx
@@ -1,17 +1,22 @@
 import store from "@/state/store";
 import { ConnectedItem } from "./ConnectedItem";
 import { TipForRelated } from "./TipForRelated";
+import { useEffect } from "react";
 
 export const SearchedItems = () => {
   const filteredElements = store.use.filteredElements();
-  console.log(filteredElements)
+  const setSelectedItemId = store.use.setSelectedItemId();
+
+  useEffect(() => {
+    setSelectedItemId(0);
+  }, [setSelectedItemId]);
 
   return (
     <ul className="space-y-1">
       <TipForRelated />
       {filteredElements.map((data, index) => {
         return (
-            <ConnectedItem data={data} index={index} />
+            <ConnectedItem key={data.id} data={data} index={index} />
         );
       })}
     </ul>
